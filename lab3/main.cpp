64ba99344dc3e1ec33672280fece00fbaf695094
magfey <126892418+magfey@users.noreply.github.com>
2025-10-08 15:23:32 +0200

    2

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,106 +1,154 @@
 #include <iostream>
-#include <numbers>
-
-
-//Лекция 3
 
 int main() {
-    /*
-    const double PI = acos(-1); // арккосинус -1 = PI 
-    double r;
-    std::cin >> r;
-    double S =PI * pow(r, 2);
-    std::cout << S << std::endl; 
-    
-    std::cout << INT_MAX << std::endl;
-    std::cout << std::numeric_limits<int>::max() << std::endl;
-    */
-
-    /* Битовый сдвиг
-    int x = 2;
-    int y = (x << 1); // Битовый сдвиг << - влево >> - вправо
-    std::cout << y << std::endl;
-    // Битовый &
-    int a = 0b0010;
-    int b = 0b1110;
-    // 0b0010
-    std::cout << (a & b) << std::endl;
-    int c = 11;
-    // 0b1011                     Проверка четности числа
-    // 0b0001
-    std::cout << (c & 1) << std::endl;
-    */
-
-    /* Битовое | (дизъюнкция)
-    int a = 0b0010;
-    int b = 0b1110;
-    // 0b1110
-    
-    // Битовая инверсия (~)
-    int d = 0b1010;
-    std::cout << ~d << std::endl; // 0b0101
-
-    // Битовый xor (^) (Исключающее или: если числа одинаковые, то 0, если разные, то 1)
-    int a = 0b1010;
-    int b = 0b1110;
-    // 0b0101
-
-    // Сдвиг на i-ый бит
-    int i = 5;
-    char lamp_status = 0b0000'0000;
-    std::cout << (lamp_status | (1 << i)) << std::endl; // сдвиг на i-ый бит
-    
-
-    int x = 11;
-    bool isEven = x % 2;
-    std::cout << isEven << std::endl;
-    */
-
-    // Логические операторы
-    // ==, !=, !, <, > , <=, >=
-    // && это логическое И
-    // || это логическое ИЛИ
-    // ! это логическое НЕ
-    /*
-    int x = 10;
-    bool f = ((x == 10) || (x++)); // short-curcuit evaluation (краткосрочная оценка)
-    std::cout << x << std::endl;*/
-    // 10
-
-    /* Условный оператор if else
-    int x = 11;
-    bool isEven = (x % 2 == 0);
-    if (isEven) {
-        if (x == 13) {
-            std::cout << "Even" << std::endl;
-        }
-        else {
-            //...
-        }
+	setlocale(LC_ALL, "RU");
+	short x; // переменная с числом
+    char enter; // переменная с ответами пользователя
+	bool x0, x1, x2, x3, x4, x5, x6, x7; // переменные с битами числа
+	std::cout << "enter number from 0 to 255: "; // просим число
+	std::cin >> x;
+	x7 = ((x >> 0) & 1);
+	x6 = ((x >> 1) & 1);
+	x5 = ((x >> 2) & 1);
+	x4 = ((x >> 3) & 1);
+	x3 = ((x >> 4) & 1);
+	x2 = ((x >> 5) & 1);
+	x1 = ((x >> 6) & 1);
+	x0 = ((x >> 7) & 1);
+	std::cout << "0b" << x0 << x1 << x2 << x3 << x4 << x5 << x6 << x7 << std::endl; // выводим число
+    std::cout << "Main power supply status: "; // выводим информацию
+    if (((x >> 0) & 1) == 1) {
+        std::cout << "Available";
     }
-    else if (x == 12) {
-        std::cout << "abcd" << std::endl;
-    }    
     else {
-        std::cout << "Odd" << std::endl;
-    }
-    */
-
-    /* Switch
-    int monthNumber = 13;
-    switch (monthNumber) {
-        case 0:std::cout << "January"; break;
-        case 1:std::cout << "February"; break;
-        // ...
-        default:std::cout << "Unknowen number"; break; // Если не попал не в один из кейсов
+        std::cout << "Not available";
     }
 
-    // Тернарый (компактный, но плохая читаемость)
-    int x = 11;
-    std::cout << (x % 2 == 0 ? "Even" : "Odd");
-    */
-return 0;
+    std::cout << "\nControl cabinet door status: ";
+    if (((x >> 2) & 1) == 1) {
+        std::cout << "Open";
+    }
+    else {
+        std::cout << "Closed";
+    }
+
+    std::cout << "\nOutput #1 status: ";
+    if (((x >> 3) & 1) == 1) {
+        std::cout << "Enabled";
+    }
+    else {
+        std::cout << "Disabled";
+    }
+
+    std::cout << "\nOutput #2 status: ";
+    if (((x >> 4) & 1) == 1) {
+        std::cout << "Enabled";
+    }
+    else {
+        std::cout << "Disabled";
+    }
+
+    std::cout << "\nBackup battery status: ";
+    if (((x >> 5) & 1) == 1) {
+        std::cout << "Discharged";
+    }
+    else {
+        std::cout << "Charged";
+    }
+
+    std::cout << "\nLast error code: ";
+    if (((x >> 6) & 1) == 0 && ((x >> 7) & 1) == 0) {
+        std::cout << "00 - No error";
+    }
+    else if (((x >> 6) & 1) == 1 && ((x >> 7) & 1) == 0) {
+        std::cout << "10 - High input voltage";
+    }
+    else if (((x >> 6) & 1) == 0 && ((x >> 7) & 1) == 1) {
+        std::cout << "01 - Low input voltage";
+    }
+    else {
+        std::cout << "11 - Reserved";
+    }
+
+    std::cout << std::endl; 
+   
+    if (((x >> 0) & 1) == 1) { // спрашиваем пользоваетеля стоит ли изменять бит
+        std::cout << "Turn off the main power? (y/n):";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 0);
+        }
+    }
+    else {
+        std::cout << "Turn on the main power? (y/n): ";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 0);
+        }
+    }
+
+    std::cout << std::endl;
+
+    if (((x >> 2) & 1) == 1) {
+        std::cout << "Close control cabinet? (y/n): ";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 2);
+        }
+    }
+    else {
+        std::cout << "Open the control cabinet? (y/n): ";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 2);
+        }
+    }
+
+    std::cout << std::endl;
+
+    if (((x >> 3) & 1) == 1) {
+        std::cout << "Turn off output 1? (y/n):";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 3);
+        }
+    }
+    else {
+        std::cout << "Turn on output 1? (y/n): ";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 3);
+        }
+    }
+
+    std::cout << std::endl;
+
+    if (((x >> 4) & 1) == 1) {
+        std::cout << "Turn off output 2? (y/n): ";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 4);
+        }
+    }
+
+    else {
+        std::cout << "Turn on output 2? (y/n): ";
+        std::cin >> enter;
+        if (enter == 'y') {
+            x ^= (1 << 4);
+        }
+    }
+    
+    x7 = ((x >> 0) & 1); // выводим новое двоичное число
+    x6 = ((x >> 1) & 1);
+    x5 = ((x >> 2) & 1);
+    x4 = ((x >> 3) & 1);
+    x3 = ((x >> 4) & 1);
+    x2 = ((x >> 5) & 1);
+    x1 = ((x >> 6) & 1);
+    x0 = ((x >> 7) & 1);
+    std::cout << "0b" << x0 << x1 << x2 << x3 << x4 << x5 << x6 << x7 << std::endl;
+    std::cout << x;
+
+    return 0;
 }
-
-
-
